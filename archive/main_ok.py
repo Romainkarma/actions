4b0000593e3fbb25caa02dfdad6fb868b9a764dc
Romain Kerampran <romain.kerampran@gmail.com>
2023-03-23 14:36:38 +0100

    jsonify

--- archive/main_ok.py
+++ archive/main_ok.py
@@ -12,19 +12,20 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     SK_ID_CURR = int(request.form['SK_ID_CURR'])
     features = app_test.loc[app_test['SK_ID_CURR'] == SK_ID_CURR]
     features = features.drop(['SK_ID_CURR'], axis=1)
     final_features = np.array(features)
-
+    
     pred_proba = model.predict_proba(features)
-    output = {'prediction': float(pred_proba[0][1])}
+    output = pred_proba[0][1]
+    
 
-    return jsonify(output)
+    return render_template('index.html', prediction_text='Le taux de probabilité que ce client ne rembourse pas est de {}'.format(output))
 
 
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(port=3000,debug=True)
